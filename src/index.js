3e480848c0cc3f2592757ababc0e5e209514c203
mayconbalves <mayconmustaine@hotmail.com>
2019-10-30 21:55:30 -0300

    feat(index): add store for all app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { Provider }from 'react-redux'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
