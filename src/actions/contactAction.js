d3d23c1e664d3da2033ab04f4a5a78d69641b33e
mayconbalves <mayconmustaine@hotmail.com>
2019-10-30 22:03:50 -0300

    feat(*): create reducer filter

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -4,3 +4,8 @@ export const createContact = contact => ({
   type: actionTypes.CREATE_NEW_CONTACT,
   contact: contact
 })
+
+export const deleteContact = id => ({
+  type: actionTypes.REMOVE_CONTACT,
+  id: id
+})
