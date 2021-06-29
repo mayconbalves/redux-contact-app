da6c09df4f767a2f4bf71efc523c2f5d084f9183
mayconbalves <mayconmustaine@hotmail.com>
2021-06-29 09:48:40 -0300

    feat: implements devtools for redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import contacts from './contactReducer';
 
 export default combineReducers({
-    contacts: contacts
+  contacts: contacts
 });
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,9 @@
-import {createStore} from 'redux';
-import rootReducer from '../reducers';
+import { createStore } from 'redux'
+import rootReducer from '../reducers'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, devTools)
 }
