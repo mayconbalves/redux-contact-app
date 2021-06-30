41175b508f29ca49f4ed7d6e4a4bbf0a6cfe25eb
mayconbalves <mayconmustaine@hotmail.com>
2021-06-30 15:16:40 -0300

    feat: implements button and input inside login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from 'components/input'
+import Button from 'components/button'
+import * as S from './styled'
 
 const Login = () => {
+  const [values, setValues] = useState({ username: '' })
+
+  const inputChange = e => {
+  }
+
+  const submitForm = () => {
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <S.Container>
+      <S.Form>
+        <h1>Repositorios do github</h1>
+        <Input
+          onChange={inputChange}
+          name="username"
+          value={values.username}
+          placeholder="Digite o seu usuário"
+        />
+        <Button
+          onClick={submitForm}
+          type="button"
+          backgroundColor="#03dac5"
+        >
+          Acessar
+        </Button>
+      </S.Form>
+    </S.Container>
   )
 }
 
