63eb52f9284ab95d9387c0712daac92b2fdb34e0
mayconbalves <mayconmustaine@hotmail.com>
2021-06-30 12:13:33 -0300

    refactor: using redux thunk inside store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux';
-import contacts from './contactReducer';
+import { combineReducers } from 'redux'
+import contacts from './contactReducer'
 
-export default combineReducers({
+const rootReducer = combineReducers({
   contacts: contacts
-});
+})
+
+export default rootReducer
