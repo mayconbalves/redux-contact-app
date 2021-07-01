7408b5277d9aab326dec38edd4131935df14e4dc
mayconbalves <mayconmustaine@hotmail.com>
2021-07-01 15:38:45 -0300

    feat: implements reducer inside store

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Input from 'components/input'
+import Button from 'components/button'
+import * as S from './styled'
+import { fetchRepositories } from './actions'
 
 const Home = () => {
-  // https://api.github.com/users/${user}/repos?per_page=50
+  const dispatch = useDispatch()
+  const [values, setRepository] = useState({ repository: ''})
+
+  const inputChange = e => {
+  }
+  
+  const submitForm = () => {
+    dispatch(fetchRepositories('mayconbalves'))
+  }
+
+  const allRepositories = []
+
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <S.Container>
+      <S.Form>
+        <h1>Digite seu nome de usuário</h1>
+        <Input
+          onChange={inputChange}
+          name="username"
+          value={values.repository}
+          placeholder="Digite o seu usuário"
+        />
+        <Button
+          onClick={submitForm}
+          type="button"
+          backgroundColor="#03dac5"
+        >
+          Acessar
+        </Button>
+      </S.Form>
+      <S.Table>
+        <thead>
+          <S.Tr>
+            <th>
+              Nome
+            </th>
+          </S.Tr>
+        </thead>
+        <tbody>
+          <S.Tr>
+            <td>React</td>
+          </S.Tr>
+        </tbody>
+      </S.Table>
+    </S.Container>
+    </>
   )
 }
 
